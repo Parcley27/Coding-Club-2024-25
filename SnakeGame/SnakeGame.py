ae3872e102c02c43f1df78f6e905c1d90bd18f33
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-10-24 16:31:28 -0700

    feat: score and highscore displays

--- SnakeGame/SnakeGame.py
+++ SnakeGame/SnakeGame.py
@@ -29,15 +29,31 @@ game_window = pygame.display.set_mode((window_width, window_height))
 
 fps = pygame.time.Clock()
 
+# Initial Game Setup
+highscore = 0
+score = 0
+
+# Display current score
+def score_display(color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render("Score: " + str(score), True, color)
+    game_window.blit(score_surface, (20, window_height - 40))
+
 # Display game controls
-def show_controls(color, font, size):
+def controls_display(color, font, size):
     controls_font = pygame.font.SysFont(font, size)
     controls_surface = controls_font.render("WASD or ARROWS to Move", True, color)
     game_window.blit(controls_surface, (495, window_height - 40))
 
+# Display highscore
+def highscore_display(color, font, size):
+    highscore_font = pygame.font.SysFont(font, size)
+    highscore_surface = highscore_font.render("Highscore: " + str(highscore), True, color)
+    game_window.blit(highscore_surface, (window_width - 110, window_height - 40))
+
 # Game loop
 while True:
-    # Able the window if you want/need
+    # Close the window if you want/need
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -51,12 +67,10 @@ while True:
         for y in range(0, grid_height, square_size):
             pygame.draw.rect(game_window, gray, pygame.Rect(x, y, square_size, square_size), 1)
 
-    # Box for bottom text
-    pygame.draw.rect(game_window, gray, pygame.Rect(0, grid_height, grid_width, grid_height))
-    pygame.draw.line(game_window, black, (0, grid_height), (grid_width, grid_height), 2)
-
-    # Display control text
-    show_controls(black, "Arial", 20)
+    # Display bottom text
+    score_display(black, "Arial", 20)
+    controls_display(black, "Arial", 20)
+    highscore_display(black, "Arial", 20)
 
     # Display the window at 60fps
     pygame.display.update()
