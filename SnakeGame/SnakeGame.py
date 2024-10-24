d395763b46941745f0c8b43a1773c28a68731fe2
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-10-24 13:10:32 -0700

    feat: add game grid and background

--- SnakeGame/SnakeGame.py
+++ SnakeGame/SnakeGame.py
@@ -5,6 +5,16 @@ import pygame
 window_x = 1200
 window_y = 600
 
+# Number of squares the grid is wide
+grid_width = 30
+
+# Square size
+square_size = window_x // 30
+
+# Define important colours in RBG values
+white = pygame.Color(255, 255, 255)
+gray = pygame.Color(200, 200, 200)
+
 # Start the game helper
 pygame.init()
 
@@ -20,7 +30,15 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            quit()
+            quit()    
+
+    # White background
+    game_window.fill(white)
+
+    # Game Grid
+    for x in range(0, window_x, square_size):
+        for y in range(0, window_y, square_size):
+            pygame.draw.rect(game_window, gray, pygame.Rect(x, y, square_size, square_size), 1)
 
     # Display the window at 60fps
     pygame.display.update()
