5091a7b9cd179b09270f790093f5d2d8a6f740cb
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2024-10-24 13:48:14 -0700

    feat: controls text label

--- SnakeGame/SnakeGame.py
+++ SnakeGame/SnakeGame.py
@@ -1,28 +1,39 @@
 # Import python game helper
 import pygame
 
-# Set window dimensions in pixels
-window_x = 1200
-window_y = 600
+# Set grid dimensions in pixels
+grid_width = 1200
+grid_height = 600
 
 # Number of squares the grid is wide
-grid_width = 30
+grid_width_in_squares = 30
 
 # Square size
-square_size = window_x // 30
+square_size = grid_width // grid_width_in_squares
+
+# Set window dimensions in pixels
+window_width = grid_width
+window_height = grid_height + 60
 
 # Define important colours in RBG values
 white = pygame.Color(255, 255, 255)
 gray = pygame.Color(200, 200, 200)
+black = pygame.Color(0, 0, 0)
 
 # Start the game helper
 pygame.init()
 
 # Create the game window
 pygame.display.set_caption("Snake")
-game_window = pygame.display.set_mode((window_x, window_y))
+game_window = pygame.display.set_mode((window_width, window_height))
 
 fps = pygame.time.Clock()
+
+# Display game controls
+def show_controls(color, font, size):
+    controls_font = pygame.font.SysFont(font, size)
+    controls_surface = controls_font.render("WASD or ARROWS to Move", True, color)
+    game_window.blit(controls_surface, (495, window_height - 40))
 
 # Game loop
 while True:
@@ -36,9 +47,16 @@ while True:
     game_window.fill(white)
 
     # Game Grid
-    for x in range(0, window_x, square_size):
-        for y in range(0, window_y, square_size):
+    for x in range(0, grid_width, square_size):
+        for y in range(0, grid_height, square_size):
             pygame.draw.rect(game_window, gray, pygame.Rect(x, y, square_size, square_size), 1)
+
+    # Box for bottom text
+    pygame.draw.rect(game_window, gray, pygame.Rect(0, grid_height, grid_width, grid_height))
+    pygame.draw.line(game_window, black, (0, grid_height), (grid_width, grid_height), 2)
+
+    # Display control text
+    show_controls(black, "Arial", 20)
 
     # Display the window at 60fps
     pygame.display.update()
